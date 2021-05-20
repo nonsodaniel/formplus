7dfa0a84c7a7e7432bb33762df6a2112a48fbde3
nonsodaniel <nonsodaniel07@gmail.com>
2021-05-20 11:44:18 +0100

    Added component layouts

--- src/components/pagination.js/Pagination.js
+++ src/components/pagination.js/Pagination.js
@@ -5,11 +5,11 @@ const Pagination = () => {
   return (
     <div className="pagination">
       <div className="pagination-wrap">
-        <h6>Previous</h6>
+        <h6 className="pagination-text">Previous</h6>
         <span>
           <button className="page-btn">1</button> of 14{" "}
         </span>
-        <h6>Next</h6>
+        <h6 className="pagination-text">Next</h6>
       </div>
     </div>
   );
--- src/components/templates/Templates.js
+++ src/components/templates/Templates.js
@@ -4,20 +4,27 @@ import "./templates.scss";
 const Templates = () => {
   return (
     <div className="wrap">
-      <h6>Templates</h6>
+      <h5 className="template-header">All Templates</h5>
       <div className="templates">
-        {[1, 2, 3, 4, 5, 6].map((o, i) => {
+    
+          {[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 3, 4,5 ].map((o, i) => {
           return (
             <div className="template" key={i}>
-              <h5>Alumni Membership Form Template</h5>
-              <p>
+              <div className="card template-card">
+                <div className="template-details">
+                  <h3>Alumni Membership Form Template</h3>
+                 <p>
                 Engage your alumni network better with this alumni registration
                 form template. Embed this in your website ...
-              </p>
-              <span>Use Template</span>
+                 </p>
+                </div>
+            
+              <div className='use-template'>Use Template</div>
+              </div>
             </div>
           );
         })}
+      
       </div>
     </div>
   );
