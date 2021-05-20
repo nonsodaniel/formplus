3b3a15697a9017a25daf256aac5561751365bf84
nonsodaniel <nonsodaniel07@gmail.com>
2021-05-20 02:21:02 +0100

    Setup Application #First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Container from "./components/container/Container";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+const App = () => {
+  return (
+      <div className="app">
+        <Container />
+      </div>
+  );
+};
+
+export default App;
---
+++ src/components/container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container = () => {
+    return (
+        <div>
+     Hello World
+        </div>
+    )
+}
+
+export default Container
